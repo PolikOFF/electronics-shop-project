be10167183951dde42dc57614895bcc2c0c96432
Andrew_Polik <polik.24@mail.ru>
2023-11-16 18:34:21 +0300

    Добавление repr и str

--- src/item.py
+++ src/item.py
@@ -74,3 +74,9 @@ class Item:
         Статический метод возвращающий число из числа строки
         """
         return int(float(number))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.__name
--- tests/test_item.py
+++ tests/test_item.py
@@ -14,12 +14,24 @@ def test_string_to_number():
     assert Item.string_to_number('2.75') == 2
     assert Item.string_to_number('2.01') == 2
 
+
 def test_name():
     item1 = Item("Буковки", 10, 2)
     assert item1.name == "Буковки"
     item1.name = "Слишкоммногобукавок"
     assert item1.name == "Слишкоммно"
 
+
 def test_instantiate_from_csv():
     Item.instantiate_from_csv('test.csv')
     assert len(Item.all) == 5
+
+
+def test_repr():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+
+def test_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
